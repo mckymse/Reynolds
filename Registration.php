34ae4d89e27fd850bcff01d49531ea79ff27e7cc
jshwnth <jashusatish@gmail.com>
2020-02-01 22:56:23 +0530

    Revert "Add files via upload"
    This reverts commit cc369e8d72ff67bae41af081410744faf0b1eace.

--- Registration.php
+++ Registration.php
@@ -1,29 +1,45 @@
 <!DOCTYPE html>
+
 <head>
     <title></title>
     <link rel="stylesheet" type="text/css" href="css.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <script>
+        function validation() {
+            var pass = document.getElementById("Password").value;
+            var cpass = document.getElementById("CPassword").value;
+            if (pass != cpass) {
+                alert('Invalid Email Address');
+                return false;
+            }
+        }
+    </script>
+
+
+
 </head>
-<body >
-        <div class="container">
-      
-                <div class="row justify-content-center">
-                    <div class="col-12 col-sm-6 col-md-3">
-                    <form class="form-container">
-                            
-                            <div class="form-group">
-                              <label for="Name" class="text-muted">Name</label>
-                              <input type="text" class="form-control" id="name" name="Name" required>
-                              
-                            </div>
-                            <div class="form-group">
-                                    <label for="email" class="text-muted">Email</label>
-                                    <input type="email" class="form-control" id="email" name="Email" required>
-                                    
-                                  </div>
-                            <div class="form-group">
-                                <label for="Department" class="text-muted">Department</label>
-                                <select class="form-control" id="Department">
+
+<body>
+    <div class="container">
+
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-6 col-md-3">
+                <form class="form-container" action="http://localhost/reynolds/Reynolds/php/register/registration.php" method="POST" onsubmit="return validation();">
+
+                    <div class="form-group">
+                        <label for="Name" class="text-muted">Name</label>
+                        <input type="text" class="form-control" id="name" name="Name" required>
+
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="text-muted">Email</label>
+                        <input type="email" class="form-control" id="email" name="Email" required>
+
+                    </div>
+                    <div class="form-group">
+                        <label for="Department" class="text-muted">Department</label>
+                        <select class="form-control" id="Department" name="department">
                                     <option>Department1</option>
                                     <option>Department2</option>
                                     <option>Department3</option>
@@ -31,28 +47,29 @@
 
                                 </select>
 
-                            </div>
-                            <div class="form-group">
-                              <label for="Password" class="text-muted">Password</label>
-                              <input type="password" class="form-control" id="Password" name="password" required>
-                            </div>
-                            <div class="form-group">
-                                    <label for="CPassword" class="text-muted">Confirm Password</label>
-                                    <input type="password" class="form-control" id="CPassword" name="Cpassword" required>
-                                  </div>
-                            
-                            
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                            <p></p>
-                            <p class="text-secondary">Existing User?<br><a href="LoginPage.html">Click here</a></p>
-                          </form>
-        
-                          </div>
-                          
-                          </div>
-                          </div>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+                    </div>
+                    <div class="form-group">
+                        <label for="Password" class="text-muted">Password</label>
+                        <input type="password" class="form-control" id="Password" name="password" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="CPassword" class="text-muted">Confirm Password</label>
+                        <input type="password" class="form-control" id="CPassword" name="Cpassword" required>
+                    </div>
+
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <p></p>
+                    <p class="text-secondary">Existing User?<br><a href="http://localhost/reynolds/Reynolds/LoginPage.php">Click here</a></p>
+                </form>
+
+            </div>
+
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
+
 </html>
