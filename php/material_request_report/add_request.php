92411e6224ef137b0037f4efea3a189f69dbb7e6
jshwnth <jashusatish@gmail.com>
2020-02-03 19:15:07 +0530

    added redirect from request to purchase and composer files

--- php/material_request_report/add_request.php
+++ php/material_request_report/add_request.php
@@ -9,9 +9,13 @@ $date = date('Y-m-d', strtotime($rawdate));
 $current_stock = mysqli_query($conn, "select quantity from item where item_id = $id");
 $row = mysqli_fetch_array($current_stock);
 if($row['quantity'] < $quantity){
-    echo " <script> window.alert('Requested stock quantity is not greater than current availablity');";
-    echo "window.location.href = 'http://localhost/reynolds/Reynolds/PurchaseList.php';";
-    echo "</script>";
+    echo "<script>
+    if (confirm('Requested stock quantity is not greater than current availablity. Do you want to place request to replace stock?')) {
+        window.location.href = 'http://localhost/reynolds/Reynolds/request_purchase.php';
+      } else {
+        window.location.href = 'http://localhost/reynolds/Reynolds/RequestList.php';
+      } 
+      </script>";
 }
 else{
     $sql = "UPDATE item SET quantity=$row[quantity]-$quantity where item_id=$id";
@@ -21,9 +25,9 @@ else{
     $desc = $get_items['desc'];
     $sql = "INSERT INTO material_request VALUES (NULL, curdate(), 4, $id, $quantity, '$date' )";
     if ($conn->query($sql) === TRUE) {
-        echo "<script>alert('Inserted successfully');";
+        echo "alert('Inserted successfully');";
         echo "window.location.href = 'http://localhost/reynolds/Reynolds/RequestList.php';";
-        echo "</script>";
+        echo "";
         }
     else{
         die(mysqli_error($conn));
